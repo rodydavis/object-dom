3ccefa634ab5915ee5465465049c4ae5fce73dee
Rody Davis <rody.davis.jr@gmail.com>
2021-03-24 13:49:36 -0700

    updating example

--- docs/app.js
+++ docs/app.js
@@ -2,27 +2,29 @@ import { ObjectDomBase, Div, Heading1, Paragraph, Button, Row, render } from './
 
 class MyApp extends ObjectDomBase {
     counter = new Paragraph({ text: '0', style: { margin: '5px' } });
-    increment = new Button({
-        text: '+',
-        style: { width: '50px', margin: '5px' },
-        onClick: () => {
-            this.counter.text = (Number(this.counter.text) + 1).toString();
-        }
-    })
-    decrement = new Button({
-        text: '-',
-        style: { width: '50px', margin: '5px' },
-        onClick: () => {
-            this.counter.text = (Number(this.counter.text) - 1).toString();
-        }
-    });
 
     build() {
         return new Div({
             children: [
                 new Heading1({ text: 'Counter Example' }),
                 this.counter,
-                new Row({ children: [this.decrement, this.increment] }),
+                new Row({
+                    children: [
+                        new Button({
+                            text: '-',
+                            style: { width: '50px', margin: '5px' },
+                            onClick: () => {
+                                this.counter.text = (Number(this.counter.text) - 1).toString();
+                            }
+                        }), new Button({
+                            text: '+',
+                            style: { width: '50px', margin: '5px' },
+                            onClick: () => {
+                                this.counter.text = (Number(this.counter.text) + 1).toString();
+                            }
+                        }),
+                    ]
+                }),
             ]
         });
     }
