be706664fbd7854e528a3dc7b77710bfbf111875
Rody Davis <rody.davis.jr@gmail.com>
2021-04-13 15:22:27 -0700

    updating example

--- example/rollup.config.ts
+++
@@ -1,8 +0,0 @@
-import html from '@web/rollup-plugin-html';
-import resolve from 'rollup-plugin-node-resolve'
-
-export default {
-  input: 'index.html',
-  output: { dir: 'dist' },
-  plugins: [html(), resolve()]
-};
